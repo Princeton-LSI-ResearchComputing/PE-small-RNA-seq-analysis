a49614e03badee8744d5aecbfa1e4448c9c11a36
Lance Parsons <lparsons@princeton.edu>
2023-03-17 21:16:56 -0400

    Coverage Plots: simplify annotation, add optional other category

--- render_quarto_reports.sh
+++ render_quarto_reports.sh
@@ -7,15 +7,10 @@ quarto render differential-expression.qmd
 
 quarto render coverage-plots.qmd \
     --to html \
-    -P normalization:human_small_rna \
-    --output coverage-plots-norm-human-small-rna.html
+    -P plot_other:FALSE \
+    --output coverage-plots-without-other.html
 
 quarto render coverage-plots.qmd \
     --to html \
-    -P normalization:exogenous_rna \
-    --output coverage-plots-norm-exogenous-rna.html
-
-quarto render coverage-plots.qmd \
-    --to html \
-    -P normalization:exogenous_rna_category \
-    --output coverage-plots-norm-exogenous-rna-category.html
+    -P plot_other:TRUE \
+    --output coverage-plots-with-other.html
